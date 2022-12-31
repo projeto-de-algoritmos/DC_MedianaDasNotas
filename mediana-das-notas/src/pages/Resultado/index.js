48d8159924c08cd5df826a37ec4071ed0e0c05a8
saracampss <saracampxx@gmail.com>
2022-12-30 23:40:35 -0300

    Estruturando pagina de resultado

--- mediana-das-notas/src/pages/Resultado/index.js
+++ mediana-das-notas/src/pages/Resultado/index.js
@@ -1,6 +1,55 @@
+import { useEffect, useState } from "react";
+import AlunoForm from "../../components/AlunoForm";
+import AlunoItem from "../../components/AlunoItem";
+import acharMediana from "../../utils/acharMediana";
+
 export default function Home()
 {
+  const [alunos, setAlunos] = useState([
+    {
+      nome: 'arthur', 
+      nota: 5
+    }, 
+    {
+      nome: 'sara', 
+      nota: 500
+    },
+    {
+      nome: 'joao', 
+      nota: 10
+    },
+    {
+      nome: 'ernesto', 
+      nota: 1
+    },
+    {
+      nome: 'olga', 
+      nota: 8
+    },
+    {
+      nome: 'valentinho', 
+      nota: 500
+    }
+  ]);
+  const [mediana, setMediana] = useState(-1);
+
+  useEffect(() => {
+    const novaMediana = acharMediana(alunos.reduce((acc, curr) => {
+      return [...acc, Number(curr.nota)];
+    }, []));
+    setMediana(novaMediana);
+  }, [alunos]);
+
   return(
-      <h1>resultado</h1>
+      <div className='page'>
+        <h1>Listagem de alunos</h1>
+        <p>Adicione os alunos e descubra o valor da mediana das notas</p>
+        <AlunoForm onPush={(aluno) => {setAlunos([...alunos, aluno])}} />
+        { alunos.map((aluno) => {
+          return(
+            <AlunoItem { ...aluno } isMediana={ mediana===aluno.nota } />
+          );
+        }) }
+      </div>
   );
 }
